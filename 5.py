bfe5cecd1de4465070246390803f553b356a87df
f1r3_r41n <qkrghkql1@gmail.com>
2024-12-02 15:29:53 +0900

    복습

--- 5.py
+++ 5.py
@@ -2,32 +2,52 @@
 peak.html
 '''
 
+# import re, pickle
+# from get_html_source import get_html
+
+# url = "http://www.pythonchallenge.com/pc/def/peak.html"
+# A = get_html(url)
+
+# with open('source.txt', 'w+') as f:
+#     source = A.get_req().replace('\n', '')
+#     m = re.findall('<!--(.+?)-->', source)
+#     m2 = re.findall('[a-z]{6}.p', source)
+#     f.write(m[0].strip()+'\n'+m2[0].strip())
+#     A.change_url(url.replace('peak.html', m2[0]))
+
+# '''
+# peak hell sounds familiar ?
+# banner.p
+# '''
+
+# with open('banner.p', 'wt') as f:
+#     f.write(A.get_req())
+
+# with open('banner.p', 'rb') as f:
+#     pfile = pickle.load(f)
+
+# with open('source.txt', 'wt') as f:
+#     for i in range(len(pfile)):
+#         for j in range(len(pfile[i])):
+#             f.write(pfile[i][j][0]*pfile[i][j][1])
+#         f.write('\n')
+
 import re, pickle
 from get_html_source import get_html
 
-url = "http://www.pythonchallenge.com/pc/def/peak.html"
-A = get_html(url)
+A = get_html('http://www.pythonchallenge.com/pc/def/peak.html')
 
-with open('source.txt', 'w+') as f:
-    source = A.get_req().replace('\n', '')
-    m = re.findall('<!--(.+?)-->', source)
-    m2 = re.findall('[a-z]{6}.p', source)
-    f.write(m[0].strip()+'\n'+m2[0].strip())
-    A.change_url(url.replace('peak.html', m2[0]))
-
-'''
-peak hell sounds familiar ?
-banner.p
-'''
+m = re.findall('[a-z]{6}.p', A.get_req())
+A.change_url(A.check_url().replace('peak.html', m[0]))
 
 with open('banner.p', 'wt') as f:
     f.write(A.get_req())
 
 with open('banner.p', 'rb') as f:
-    pfile = pickle.load(f)
-
-with open('source.txt', 'wt') as f:
-    for i in range(len(pfile)):
-        for j in range(len(pfile[i])):
-            f.write(pfile[i][j][0]*pfile[i][j][1])
+    pf = pickle.load(f)
+    
+with open('result.txt', 'wt') as f:
+    for i in range(len(pf)):
+        for j in range(len(pf[i])):
+            f.write(pf[i][j][0]*pf[i][j][1])
         f.write('\n')
--- 6.py
+++ 6.py
@@ -2,58 +2,82 @@
 channel.html
 '''
 
+# import re, zipfile
+# from get_html_source import get_html
+
+# url = "http://www.pythonchallenge.com/pc/def/channel.html"
+# A = get_html(url)
+
+# with open('source.txt', 'wt') as f:
+#     source = A.get_req().replace('\n', '')
+#     m = re.findall('<!--(.+?)-->', source)
+#     for i in m:
+#         f.write(i.strip()+'\n')
+
+# '''
+# <-- zip
+
+# The following has nothing to do with the riddle itself. 
+# I just thought it would be the right point to offer you to donate to thePython Challenge project. 
+# Any amount will be greatly appreciated.
+# -thesamet
+# '''
+
+# url = url.replace('html', 'zip')
+# A.change_url(url)
+
+# with open('channel.zip', 'wb') as f:
+#     f.write(A.get_content())
+
+# with open('channel/readme.txt', 'rt') as f:
+#     msg = f.read().replace('\n', '')
+#     num = re.findall('(\d{5})', msg)[0]
+#     print(num)
+
+# '''
+# welcome to my zipped list.
+
+# hint1: start from 90052
+# hint2: answer is inside the zip
+# '''
+
+# ZF = zipfile.ZipFile('channel.zip')
+
+# while num:
+#     FILE = f'channel/{num}.txt'
+#     with open(FILE, 'rt') as f:
+#         txt = f.read()
+#         num = ''.join(re.findall('(\d+?)', txt))
+#         if not num:
+#             '''
+#             Collect the comments.
+#             '''
+#             break
+#         print(ZF.getinfo(FILE.replace('channel/', '')).comment.decode('UTF-8'), end='')
+#         '''
+#         extract comments from zip file => getinfo('file_name').comment
+#         '''
+
 import re, zipfile
 from get_html_source import get_html
 
-url = "http://www.pythonchallenge.com/pc/def/channel.html"
-A = get_html(url)
-
-with open('source.txt', 'wt') as f:
-    source = A.get_req().replace('\n', '')
-    m = re.findall('<!--(.+?)-->', source)
-    for i in m:
-        f.write(i.strip()+'\n')
-
-'''
-<-- zip
-
-The following has nothing to do with the riddle itself. 
-I just thought it would be the right point to offer you to donate to thePython Challenge project. 
-Any amount will be greatly appreciated.
--thesamet
-'''
-
-url = url.replace('html', 'zip')
-A.change_url(url)
+A = get_html('http://www.pythonchallenge.com/pc/def/channel.html')
+p1 = re.compile('<!--(.+?)-->')
+p2 = re.compile('[a-z]+')
+A.change_url(A.check_url().replace(
+    'html',
+    p2.findall(p1.findall(A.get_req().replace('\n', ''))[0])[0]
+))
 
 with open('channel.zip', 'wb') as f:
     f.write(A.get_content())
+    
+# import os
+# os.system("unzip channel.zip -d channel/")
 
 with open('channel/readme.txt', 'rt') as f:
-    msg = f.read().replace('\n', '')
-    num = re.findall('(\d{5})', msg)[0]
+    num = re.findall(
+        '(\d{5})',
+        f.read().replace('\n', '')
+    )[0]
     print(num)
-
-'''
-welcome to my zipped list.
-
-hint1: start from 90052
-hint2: answer is inside the zip
-'''
-
-ZF = zipfile.ZipFile('channel.zip')
-
-while num:
-    FILE = f'channel/{num}.txt'
-    with open(FILE, 'rt') as f:
-        txt = f.read()
-        num = ''.join(re.findall('(\d+?)', txt))
-        if not num:
-            '''
-            Collect the comments.
-            '''
-            break
-        print(ZF.getinfo(FILE.replace('channel/', '')).comment.decode('UTF-8'), end='')
-        '''
-        extract comments from zip file => getinfo('file_name').comment
-        '''
